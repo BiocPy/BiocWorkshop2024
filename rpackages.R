dacc2055bf0f14dac20efee27bdaf437672be753
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2024-07-16 16:23:50 -0700

    Update tutorials (#5)

--- rpackages.R
+++ rpackages.R
@@ -1,3 +1,6 @@
 install.packages(c("BiocManager"), repos='http://cran.us.r-project.org')
 library(BiocManager)
-BiocManager::install(c("scRNAseq", "celldex", "SingleR", "scuttle", "reticulate", "rmarkdown", "knitr", "downlit", "xml2", "ggplot2", "edgeR"))
+BiocManager::install(
+    c("scRNAseq", "celldex", "SingleR", "scuttle", "reticulate", 
+    "rmarkdown", "knitr", "downlit", "xml2", "ggplot2", "edgeR", 
+    "AnnotationHub", "TxDb.Hsapiens.UCSC.hg38.refGene"))
