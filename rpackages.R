03b4ec17487b4c3197ab6a110ad66445681931b2
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2024-07-17 08:15:02 -0700

    more edits

--- rpackages.R
+++ rpackages.R
@@ -3,4 +3,4 @@ library(BiocManager)
 BiocManager::install(
     c("scRNAseq", "celldex", "SingleR", "scuttle", "reticulate", 
     "rmarkdown", "knitr", "downlit", "xml2", "ggplot2", "edgeR", 
-    "AnnotationHub", "TxDb.Hsapiens.UCSC.hg38.refGene"))
+    "AnnotationHub"))
